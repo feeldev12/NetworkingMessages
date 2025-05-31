28dfc1915ca65667868d0e62c5c6f4a93d6b2f90
FrancoBM <baldeonjean@gmail.com>
2025-05-31 14:22:03 -0500

    feat: configure to publish API

--- common/src/main/java/me/feeldev/networking/CommonAPI.java
+++ common/src/main/java/me/feeldev/networking/CommonAPI.java
@@ -4,6 +4,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class CommonAPI {
-    private static final String MOD_ID = "NetworkingMessages";
+    private static final String MOD_ID = "networking_messages";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 }
