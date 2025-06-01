a1537c13435d865aa91caf5988448e054b7e772b
FrancoBM <baldeonjean@gmail.com>
2025-06-01 17:18:41 -0500

    feat: configure to publish API v2.9

--- client/src/main/java/me/feeldev/networking/client/models/AbstractMessage.java
+++ client/src/main/java/me/feeldev/networking/client/models/AbstractMessage.java
@@ -15,12 +15,6 @@ public abstract class AbstractMessage<T extends AbstractMessage<T>> implements I
         this.messageType = messageType;
     }
 
-    public AbstractMessage() {
-
-    }
-
-
-
     public MessageType getMessageType() {
         return messageType;
     }
