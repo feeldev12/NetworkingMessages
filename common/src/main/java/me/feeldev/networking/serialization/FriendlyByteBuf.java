9fe3782f5cd762af50f135531d164b454257f72b
feeldev12 <ferbmax120@gmail.com>
2025-07-25 15:26:07 -0500

    fix FriendlyByteBuf method and added utility for debug packet stats

--- common/src/main/java/me/feeldev/networking/serialization/FriendlyByteBuf.java
+++ common/src/main/java/me/feeldev/networking/serialization/FriendlyByteBuf.java
@@ -37,23 +37,16 @@ public class FriendlyByteBuf extends ByteBuf {
     }
 
     public void detectCompression() {
-        Class<?> clazz;
-        try {
-            clazz = Class.forName("me.feeldev.networking.server.ServerAPI");
-        } catch (ClassNotFoundException ex) {
-            try {
-                clazz = Class.forName("me.feeldev.networking.client.ClientAPI");
-            } catch (ClassNotFoundException ignored) {
-                CommonAPI.LOGGER.warn("Could not detect compression");
-                return;
-            }
+        NetworkAPI<?> api = CommonAPI.getNetworkAPI();
+        if(api == null) {
+            CommonAPI.LOGGER.warn("Could not detect network api");
+            return;
         }
-        NetworkAPI<?> api = (NetworkAPI<?>) clazz.cast(NetworkAPI.class);
         this.compressed = api.isCompressionEnabled();
     }
 
-    public void enableCompression(boolean compressed) {
-        this.compressed = compressed;
+    public void enableCompression() {
+        this.compressed = true;
     }
 
     public void disableCompression() {
